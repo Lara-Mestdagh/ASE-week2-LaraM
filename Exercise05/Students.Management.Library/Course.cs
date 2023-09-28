be1dcb7ec25d6a1825f298728de7a4df5a8fcce9
Lara Mestdagh <lara.mestdagh@student.howest.be>
2023-09-28 16:09:22 +0200

    complete

---
+++ Exercise05/Students.Management.Library/Course.cs
@@ -0,0 +1,29 @@
+namespace MCT.Ex4.Models.Course;
+public class Course
+{
+    // CONSTRUCTORS
+    public Course(int id, string name, int pricepersemester)
+    {
+        ID = id;
+        Name = name;
+        PricePerSemester = pricepersemester;
+    }
+
+    // FUNCTIONS
+    public override string ToString()
+    {
+        // Build a string representation of the person's information
+        var info = "Course Information:\n";
+        info += $"ID: {ID}\n";
+        info += $"Name: {Name}\n";
+        info += $"Price/Semester: {PricePerSemester}\n";
+
+        return info;
+    }
+
+    // PROPERTIES
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public int PricePerSemester { get; set; }
+
+}
